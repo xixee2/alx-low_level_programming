fa55d572921c189de4fe008d842d9c0320919acd
zainab wumi <xixee1121@gmail.com>
2022-09-22 15:35:45 -0700

    2-strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,26 +8,26 @@
  *
  * Return: char value
  */
- char *_strncpy(char *dest, char *src, int n)
- {
-	 int a = 0, b = 0;
-	 
-	 while (src[b])
-	 {
-		 b++;
-	 }
-	 
-	 while (a < n && src[a])
-	 {
-		 dest[a] = src[a];
-		 a++;
-	 }
-	 
-	 while (a < n)
-	 {
-		 dest[a] = '\0';
-		 a++;
-	 }
-	 
-	 return (dest);
- }
+char *_strncpy(char *dest, char *src, int n)
+{
+	int a = 0, b = 0;
+
+	while (src[b])
+	{
+		b++;
+	}
+
+	while (a < n && src[a])
+	{
+		dest[a] = src[a];
+		a++;
+	}
+
+	while (a < n)
+	{
+		dest[a] = '\0';
+		a++;
+	}
+	
+	return (dest);
+}

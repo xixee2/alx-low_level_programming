3a3b70178708cf21695caf9716a3a01ab7cbe172
zainab wumi <xixee1121@gmail.com>
2022-09-27 15:04:20 -0700

    0-memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,8 +1,20 @@
-#ifndef FILE_MAIN
-#define FILE_MAIN
+#include "main.h"
+/**
+ * _memset - function
+ * @s : the address of the memory we want to filled it u
+ * @b : the caracter that will fill the memory
+ * @n : the number of times we will fill the memory
+ * Return: memory.
+ */
 
-#include <stddef.h>
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
 
-int _putchar(char c);
-char *_memset(char *s, char b, unsigned int n);
-
+	while (i < n)
+	{
+		s[i] = b;
+		i++;
+	}
+	return (s);
+}

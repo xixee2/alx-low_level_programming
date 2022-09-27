e960b4c3856bdbf628e55d318a036ea1baf429f1
zainab wumi <xixee1121@gmail.com>
2022-09-27 14:52:01 -0700

    main.h

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,8 @@
+#ifndef FILE_MAIN
+#define FILE_MAIN
+
+#include <stddef.h>
+
+int _putchar(char c);
+char *_memset(char *s, char b, unsigned int n);
+
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+More string array and pointer

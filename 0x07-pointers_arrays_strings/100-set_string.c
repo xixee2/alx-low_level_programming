597cd9a478413b6b3d6a6802d5c790cc0ea1a52b
zainab wumi <xixee1121@gmail.com>
2022-09-27 17:54:34 -0700

    100-set

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * set_string - check the code for Holberton School students.
+ * *@s : variable
+ * *@to : variable
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}

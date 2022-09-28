094c4b4c95267ba6ab7ded5e2c5d012b944330b9
zainab wumi <xixee1121@gmail.com>
2022-09-27 17:03:24 -0700

    4-strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -5,24 +5,23 @@
  * _strpbrk - searches string for trace of a set of bytes
  * @s: string
  * @accept: string containing bytes to look for
- *
  * Return: pointer to byte
  */
 char *_strpbrk(char *s, char *accept)
 {
 	int i, j;
-
+	
 	for (i = 0; *s != '\0'; i++)
 	{
 		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (*s == accept[j])
 			{
-			return (s);
+				return (s);
+			}
 		}
-	}
 		s++;
 	}
 
-	return (NULL);
-}		
+	return (0);
+}

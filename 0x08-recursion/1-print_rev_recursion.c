a65fa38b2706eb24a6aed85e548e87edfa44d1c2
zainab wumi <xixee1121@gmail.com>
2022-09-28 18:09:37 -0700

    1-print

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,14 @@
+#include <string.h>
+
+/**
+ * _print_rev_recursion - check the code for Holberton School students.
+ * @s : parameter
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion("\nFirst, solve the problem. Then, write the code");
+	}
+}

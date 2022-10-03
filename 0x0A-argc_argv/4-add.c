314769464ad0c46bc8e17d6b0ceb07bbdb4bc761
zainab wumi <xixee1121@gmail.com>
2022-10-03 13:43:50 -0700

    4add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,27 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+
 /**
- * main - print the result of the multiplication, followed by a new line
- * @argc: int
- * @argv: list
- * Return: 0
+ * main - adds numbers
+ * @argc: number of arguments passed to the function
+ * @argv: argument vector of pointers to strings
+ *
+ * Return: 0 if no errors, else 1
  */
-int main(int argc, char const *argv[])
+int main(int argc, char *argv[])
 {
-	int i, j, sum;
+	int a = 0, i, j;
+
 	for (i = 1; i < argc; i++)
 	{
-		for (j = 0; argv[1][j] != '\0'; j++)
+		for (j = 0; argv[i][j]; j++)
 		{
 			if (isdigit(argv[i][j]) == 0)
 			{
-				printf("Error\n");
-				return (-1);
-			        }
-		                }
-	                        sum += atoi(argv[i]);
-	    }
-            printf("%d\n", sum);
-            return (0);
+				puts("Error");
+				return (1);
+			}
+		}
+	}
+	for (i = 1; i < argc; i++)
+	{
+		a += atoi(argv[i]);
+	}
+	printf("%d\n", a);
+	return (0);
 }
+	

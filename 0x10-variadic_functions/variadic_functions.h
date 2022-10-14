9fc9772fbf4c9b74604969525c2edeaa9c6ec151
zainab wumi <xixee1121@gmail.com>
2022-10-13 18:21:53 -0700

    headerfile

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -15,11 +15,13 @@ typedef struct printer
 	char *symbol;
 	void (*print)(va_list arg);
 }printer_t;
-
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
-void print_numbers(const char *separator, const unsigned int n, ...);
-void print_strings(const char *separator, const unsigned int n, ...);
-void print_all(const char * const format, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);void print_strings(const char *separator, const unsigned int n, ...);void print_all(const char * const format, ...);
 
+
+void t_char(va_list va);
+void t_integer(va_list va);
+void t_float(va_list va);
+void t_string(va_list va);
 #endif /* VARIADIC_FUNCTIONS_H */

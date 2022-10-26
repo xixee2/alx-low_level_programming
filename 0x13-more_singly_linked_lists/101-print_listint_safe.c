1a4d38a290f3282639ca5a34310d23713f9bcaf2
zainab wumi <xixee1121@gmail.com>
2022-10-25 18:19:43 -0700

    safe

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+ * print_listint_safe - define function
+ * @head: descrie argument
+ * Return: what does it return?
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	head++;
+	return (0);
+}

6db960328f82250cd67c38e9aea3af4f66faf7d9
zainab wumi <xixee1121@gmail.com>
2022-10-25 18:32:47 -0700

    safe

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,11 +1,28 @@
 #include "lists.h"
+
 /**
- * print_listint_safe - define function
- * @head: descrie argument
- * Return: what does it return?
+ * reverse_listint - reverses a listint_t linked list.
+ * @head: double pointer to the start of the list
+ *
+ * Return: a pointer to the first node of the reversed list
  */
-size_t print_listint_safe(const listint_t *head)
+listint_t *reverse_listint(listint_t **head)
 {
-	head++;
-	return (0);
+	listint_t *previous, *next;
+
+	if (head == NULL || *head == NULL)
+		return (NULL);
+	if ((*head)->next == NULL)
+		return (*head);
+	previous = NULL;
+	while (*head != NULL)
+	{
+		next = (*head)->next;
+		(*head)->next = previous;
+		previous = *head;
+		*head = next;
+	}
+	*head = previous;
+	return (*head);
+
 }
